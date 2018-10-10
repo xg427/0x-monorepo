623a0d4615ee3998668ce3f5a590fd291d34c424
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2018-10-10 12:14:20 -0400

    feat: project stub for python order utilities
    An empty project, with respect to functionality, but one configured with
    a test framework, linters, CI integration, etc.
    
    https://app.asana.com/0/836857747873847/839549782781239/f

---
+++ python-packages/order_utils/src/zero_x/order_utils/signature_utils.py
@@ -0,0 +1,10 @@
+"""Signature utilities."""
+
+
+def ec_sign_order_hash():
+    """Signs an orderHash.
+
+    Returns its elliptic curve signature and signature type.  This method
+    currently supports TestRPC, Geth, and Parity above and below v1.6.6.
+    """
+    return "stub return value"
